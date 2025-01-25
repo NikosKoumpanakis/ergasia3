7afabdad342821317419a1cb6dd7fe33136afdfa
Koukiss <nikolaskoumpa@gmail.com>
2025-01-25 17:22:32 +0200

    Create clients.c

---
+++ clients.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[256];
+
+    // Create the client socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up the server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive and print responses from the server
+    while (recv(client_socket, buffer, sizeof(buffer), 0) > 0) {
+        printf("Server: %s\n", buffer);
+    }
+
+    // Close the socket
+    close(client_socket);
+    return 0;
+}
